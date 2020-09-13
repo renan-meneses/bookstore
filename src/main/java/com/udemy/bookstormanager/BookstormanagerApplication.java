02044aecf42092417b93c1ed03faff18afd90853
Renan Meneses <renan.meneses@alu.ufc.br>
2020-09-13 00:28:49 -0300

    iniciando o projeto

---
+++ src/main/java/com/udemy/bookstormanager/BookstormanagerApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.bookstormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstormanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstormanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/bookstormanager/BookstormanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.bookstormanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstormanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
