bdcc26c63e37639879c417425fa1ff1c744ac503
Renan Meneses <renan.meneses@alu.ufc.br>
2020-09-25 05:17:11 -0300

    update: corringindo funcoes, adicionando novas funcionalidades

--- src/main/java/com/udemy/bookstormanager/dto/BookDTO.java
+++ src/main/java/com/udemy/bookstormanager/dto/BookDTO.java
@@ -32,8 +32,8 @@ public class BookDTO {
 	private Integer chapters;
 	@NotBlank
 	@Size(max=100)
-	@Pattern (regexp ="(?:ISBN(?:-10)?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$)[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$",
-      		message = "ISBN format must be a valid format")
+//	@Pattern (regexp ="(?:ISBN(?:-10)?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$)[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$",
+//      		message = "ISBN format must be a valid format")
 	private String isbn;
 	@NotBlank
 	@Size(max=200)
